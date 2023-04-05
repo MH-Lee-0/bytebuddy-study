acfd8642b626cd4773e30deea98f2d998d91581e
menghang.li <menghang.li@qunar.com>
2023-04-05 21:29:35 +0800

    创建子模块

---
+++ bytebuddy-demo/src/main/java/com/study/bytebuddy/Main.java
@@ -0,0 +1,12 @@
+package com.study.bytebuddy;
+
+/**
+ * @Author: menghang.li
+ * @date: 2023/4/5 21:28
+ * @description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
